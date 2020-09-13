4b72711bbfd9af421775e0d145f618d476945a3e
AndreasGassmann <andreas@andreasgassmann.ch>
2020-09-13 22:18:12 +0000

    feat(nx): add empty projects

---
+++ apps/knoxpass-landing/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'knoxpass-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'knoxpass-landing';
+}
