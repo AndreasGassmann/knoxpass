669f9ec11aece42f9713d4eb218be26ec6d07064
AndreasGassmann <andreas@andreasgassmann.ch>
2020-09-16 22:22:10 +0000

    feat(utils): fix lint errors

--- libs/api-interfaces/src/lib/api-interfaces.ts
+++ libs/api-interfaces/src/lib/api-interfaces.ts
@@ -3,13 +3,15 @@ export interface Message {
 }
 
 export const generateUuid = function () {
-  const buf = new Uint32Array(4);
+  const buf: Uint32Array = new Uint32Array(4);
   window.crypto.getRandomValues(buf);
   let idx = -1;
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
     idx++;
+    /* tslint:disable:no-bitwise */
     const r = (buf[idx >> 3] >> ((idx % 8) * 4)) & 15;
-    const v = c == 'x' ? r : (r & 0x3) | 0x8;
+    const v = c === 'x' ? r : (r & 0x3) | 0x8;
+    /* tslint:enable:no-bitwise */
     return v.toString(16);
   });
 };
