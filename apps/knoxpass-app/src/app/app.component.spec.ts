a1a7ca8ba9050450bdcc6fde08fdf9720b50c11c
AndreasGassmann <andreas@andreasgassmann.ch>
2020-09-20 16:16:44 +0200

    fix(extension): tests

--- apps/knoxpass-app/src/app/app.component.spec.ts
+++ apps/knoxpass-app/src/app/app.component.spec.ts
@@ -19,19 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'knoxpass-extension'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('knoxpass-extension');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain(
-      'Welcome to knoxpass-extension!'
-    );
-  });
 });
