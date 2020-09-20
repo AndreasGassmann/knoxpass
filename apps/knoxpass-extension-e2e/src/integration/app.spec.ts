858245a0438ed1621edb3b7bc763c8840a198340
AndreasGassmann <andreas@andreasgassmann.ch>
2020-09-20 03:19:45 +0200

    fix(extension): e2e test

--- apps/knoxpass-extension-e2e/src/integration/app.spec.ts
+++ apps/knoxpass-extension-e2e/src/integration/app.spec.ts
@@ -8,6 +8,6 @@ describe('knoxpass-extension', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to knoxpass-extension!');
+    getGreeting().contains('Welcome to');
   });
 });
